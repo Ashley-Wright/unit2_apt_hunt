c78590428202ffb2cc8e47491de131a1678c8812
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-01-22 00:07:20 -0600

    Tests for entering apartment complexes

--- test/test_entering_complexes.rb
+++ test/test_entering_complexes.rb
@@ -1,7 +1,43 @@
-require 'minitest/autorun'
+require_relative 'helper'
 
 class TestEnteringComplexes < MiniTest::Unit::TestCase
-  def test_complex
-    assert false
+  def test_error_message_missing_name
+    command = "./apt_hunter add complex"
+    expected = "You must provide the name of the apartment complex"
+    assert_command_output expected, command
+  end
+
+  def test_error_message_missing_zip
+    command = "./apt_hunter add complex --name 'garden terrace'"
+    expected = "You must provide the zip code of the apartment complex"
+    assert_command_output expected, command
+  end
+
+  def test_error_message_missing_parking
+    command = "./apt_hunter add complex --name 'garden terrace' --zip 37075"
+    expected = "You must provide the parking type of the apartment complex"
+    assert_command_output expected, command
+  end
+
+  def test_error_message_missing_website
+    command = "./apt_hunter add complex --name 'garden terrace' --zip 37075 --parking covered"
+    expected = "You must provide the website of the apartment complex"
+    assert_command_output expected, command
+  end
+
+  def test_error_message_missing_phone_number
+    command = "./apt_hunter add complex --name 'garden terrace' --zip 37075 --parking covered --website 'www.gardenterrace.com'"
+    expected = "You must provide the zip code of the apartment complex"
+    assert_command_output expected, command
+  end
+
+  def test_valid_complex_saved
+    skip
+    assert false, "Missing test implementation"
+  end
+
+  def test_invalid_complex_not_saved
+    skip
+    assert false, "Missing test implementation"
   end
 end
