bcd974fb1b46325e40bd985f0f901256f38e6dfe
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-01-23 23:12:29 -0600

    Error messages for adding complexes

--- test/test_entering_complexes.rb
+++ test/test_entering_complexes.rb
@@ -3,35 +3,31 @@ require_relative 'helper'
 class TestEnteringComplexes < MiniTest::Unit::TestCase
   def test_error_message_missing_name
     command = "./apt_hunter add complex"
-    expected = "You must provide the name of the apartment complex"
+    expected = "You must provide the name, zip code, parking type, website and phone number of the apartment complex."
     assert_command_output expected, command
   end
 
   def test_error_message_missing_zip
-    skip
     command = "./apt_hunter add complex --name 'garden terrace'"
-    expected = "You must provide the zip code of the apartment complex"
+    expected = "You must provide the zip code, parking type, website and phone number of the apartment complex."
     assert_command_output expected, command
   end
 
   def test_error_message_missing_parking
-    skip
     command = "./apt_hunter add complex --name 'garden terrace' --zip 37075"
-    expected = "You must provide the parking type of the apartment complex"
+    expected = "You must provide the parking type, website and phone number of the apartment complex."
     assert_command_output expected, command
   end
 
   def test_error_message_missing_website
-    skip
     command = "./apt_hunter add complex --name 'garden terrace' --zip 37075 --parking covered"
-    expected = "You must provide the website of the apartment complex"
+    expected = "You must provide the website and phone number of the apartment complex."
     assert_command_output expected, command
   end
 
   def test_error_message_missing_phone_number
-    skip
     command = "./apt_hunter add complex --name 'garden terrace' --zip 37075 --parking covered --website 'www.gardenterrace.com'"
-    expected = "You must provide the zip code of the apartment complex"
+    expected = "You must provide the phone number of the apartment complex."
     assert_command_output expected, command
   end
 
