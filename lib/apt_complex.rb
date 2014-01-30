af9d10f59e6541d1ffc1b5ab6eef3acbe2d9eb3e
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-01-29 22:06:42 -0600

    View test passing

--- lib/apt_complex.rb
+++ lib/apt_complex.rb
@@ -1,23 +1,31 @@
 require_relative 'environment'
 
 class AptComplex
-  def initialize (name, zip, parking, website, phone, environment)
-    @db = Environment.database_connection(environment)
-    @name = name
-    @zip = zip
-    @parking = parking
-    @website = website
-    @phone = phone
+  attr_accessor :name, :zip, :parking, :website, :phone
+
+  def initialize attributes = {}
+    attributes.each_pair do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
   end
 
-  def create
-    statement = "insert into complexes(name, zip, parking, website, phone) values('#{@name}', #{@zip}, '#{@parking}', '#{@website}', '#{@phone}')"
-    @db.execute(statement)
+  def create (environment)
+    statement = "insert into complexes(name, zip, parking, website, phone) values('#{name}', #{zip}, '#{parking}', '#{website}', '#{phone}')"
+    db = Environment.database_connection(environment)
+    db.execute(statement)
   end
 
   def self.view (environment)
     statement = "select name, zip, parking, website, phone from complexes"
     db = Environment.database_connection(environment)
+    db.results_as_hash = true
     results = db.execute(statement)
+    results.map do |row_hash|
+      AptComplex.new(name: row_hash["name"], zip: row_hash["zip"], parking: row_hash["parking"], website: row_hash["website"], phone: row_hash["phone"])
+    end
+  end
+
+  def to_s
+    "#{name}:   #{zip}   #{parking}   #{website}   #{phone}"
   end
 end
--- test/test_viewing_complexes.rb
+++ test/test_viewing_complexes.rb
@@ -9,15 +9,14 @@ class TestViewingComplexes < AptHuntTest
     shell_output = `./apt_hunter view complex --environment test`
     # expected = [["Garden Terrace", 37075, "garage", "www.gardenterrace.com", "555-555-5555"], ["Oak Village", 37075, "garage", "www.hickoryvillage.com", "555-555-5556"], ["Eagle Point", 37075, "uncovered", "www.eaglepoint.com", "555-555-5557"]]
     expected = <<EOS
-Garden Terrace     37075     garage     www.gardenterrace.com     555-555-5555
-Oak Village     37075     garage     www.oakvillage.com     555-555-5556
-Eagle Point     37075     uncovered     www.eaglepoint.com     555-555-5557
+Garden Terrace:   37075   garage   www.gardenterrace.com   555-555-5555
+Oak Village:   37075   garage   www.oakvillage.com   555-555-5556
+Eagle Point:   37075   uncovered   www.eaglepoint.com   555-555-5557
 EOS
     assert_equal expected, shell_output
   end
 
   def test_viewing_complexes_empty_table
-    skip
     results = database.execute("select name, zip, parking, website, phone from complexes")
     assert_equal [], results
   end
