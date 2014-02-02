8e7db12188ff4258c433664483724c3ef3ebbc3d
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-02-01 23:20:59 -0600

    Moved validate method into AptComplex class

--- lib/argument_parser.rb
+++ lib/argument_parser.rb
@@ -31,21 +31,21 @@ class Parser
     options
   end
 
-  def self.validate options
-    missing_arguments = []
-    missing_arguments << "name" unless options[:name]
-    missing_arguments << "zip code" unless options[:zip]
-    missing_arguments << "parking type" unless options[:parking]
-    missing_arguments << "website" unless options[:website]
-    missing_arguments << "phone number" unless options[:phone]
+  # def self.validate options
+  #   missing_arguments = []
+  #   missing_arguments << "name" unless options[:name]
+  #   missing_arguments << "zip code" unless options[:zip]
+  #   missing_arguments << "parking type" unless options[:parking]
+  #   missing_arguments << "website" unless options[:website]
+  #   missing_arguments << "phone number" unless options[:phone]
 
-    unless missing_arguments.empty?
-      if missing_arguments.length == 1
-        error = "You must provide the #{missing_arguments.last} of the apartment complex."
-      else
-        error = "You must provide the #{missing_arguments[0..-2].join(", ") + " and"} #{missing_arguments.last} of the apartment complex."
-      end
-      error
-    end
-  end
+  #   unless missing_arguments.empty?
+  #     if missing_arguments.length == 1
+  #       error = "You must provide the #{missing_arguments.last} of the apartment complex."
+  #     else
+  #       error = "You must provide the #{missing_arguments[0..-2].join(", ") + " and"} #{missing_arguments.last} of the apartment complex."
+  #     end
+  #     error
+  #   end
+  # end
 end
