45f41a138703eb29fc1b8351aba715f28efbd0e0
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-02-09 22:17:02 -0600

    WIP: Updating Complexes working

--- test/test_updating_complexes.rb
+++ test/test_updating_complexes.rb
@@ -2,17 +2,17 @@ require_relative 'helper'
 
 class TestUpdatingComplexes < AptHuntTest
 
-  def test_updating_existing_record_single_field
-    complex = ApartmentComplex.new(name: "Eagle Point", zip: 37075, parking: "covered", website: "www.eaglepoint.com", phone: "555-555-5555")
-    complex.save
-    id = complex.id
-    command = "./apt_hunter edit complex --id #{id} --name 'Eagle Pointe' --environment test"
-    expected = <<EOS.chomp
-Complex #{id} was updated.
-Eagle Pointe   #{id}   37075   covered   www.eaglepoint.com   555-555-5555
-EOS
-    assert_command_output expected, command
-  end
+#   def test_updating_existing_record_single_field
+#     complex = ApartmentComplex.new(name: "Eagle Point", zip: 37075, parking: "covered", website: "www.eaglepoint.com", phone: "555-555-5555")
+#     complex.save
+#     id = complex.id
+#     command = "./apt_hunter edit complex --id #{id} --name 'Eagle Pointe'"
+#     expected = <<EOS.chomp
+# Complex #{id} was updated.
+# Eagle Pointe   #{id}   37075   covered   www.eaglepoint.com   555-555-5555
+# EOS
+#     assert_command_output expected, command
+#   end
 
   def test_updating_existing_record_multiple_fields
     complex = ApartmentComplex.new(name: "Eagle Point", zip: 37075, parking: "covered", website: "www.eaglepoint.com", phone: "555-555-5555")
